92095ac680ece5415795db3016a26e3f9469818a
Arjungehlot552 <arjungehlot552@gmail.com>
2023-06-30 03:03:06 +0530

    Number to string string to number

---
+++ tut8.js
@@ -0,0 +1,27 @@
+// typeof operator 
+
+// data types (primitive data types)
+// string "arjun"
+// number 2, 4, 5.5
+// boolens
+// underfined
+// null
+// BigInt
+// Symbol
+
+// let age = 22;
+// let firstName = "arjun";
+// console.log(typeof age);
+
+// convert number to string
+// console.log(typeof (age + ""));
+// age = age + "";
+
+// convert string to number.
+
+// let myStr = +"20";
+// console.log(typeof myStr);
+
+let age = "10";
+age = Number(age);
+console.log(typeof age);
