91d602a91fd8c741a4694f0b42438fd4b143db7e
Arjungehlot552 <arjungehlot552@gmail.com>
2023-07-03 22:38:29 +0530

    java script

---
+++ tut18.js
@@ -0,0 +1,25 @@
+// if
+// else if
+// else if 
+// else if 
+// else  
+let tempInDegree = 40;
+
+if(tempInDegree < 0){
+    console.log("extremely cold outside");
+}
+else if(tempInDegree < 16){
+    console.log("It is cold outside");
+}
+else if(tempInDegree < 25){
+   console.log("weather is ohk");
+}
+else if (tempInDegree <35){
+    console.log("lets go to swim");
+}
+else if (tempInDegree <45){
+    console.log("turn on AC");
+}
+else{
+    console.log("too hot");
+}
---
+++ tut20.js
@@ -0,0 +1,25 @@
+// Intro to arrays
+// Reference type
+// How to create arrays
+
+// Ordered collection of items
+
+// let fruits = ["Apple", "Mango", "Grapes"];
+// let numbers = [1,2,3,4];
+// let mixed = [1,2,2,3, "string",null,undefined];
+// console.log(mixed);
+// console.log(numbers);
+// console.log(fruits[2]);
+
+// let fruits = ["Apple", "Mango", "Grapes"];
+// console.log(fruits);
+// fruits[1] = "banana";
+// console.log(fruits);
+
+let fruits = ["Apple", "Mango", "Grapes"];
+let obj = {};
+
+console.log(typeof fruits);
+console.log(typeof obj);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
